74c11e359f09ece9f929077ba1e79f5d372703ee
xofiaxinha <msofia.lima@alu.ufc.br>
2025-02-23 10:47:30 -0300

    alteração nos elitismos pro novo formato

--- def_elitismos.py
+++ def_elitismos.py
@@ -1,6 +1,6 @@
 import random
 
-def elitismo_simples(populacao, fitness):
+def simples(populacao, fitness):
     '''
     faz uma seleção ordenada pelo fitness onde metade da população atual vai
     para a próxima geração
@@ -12,7 +12,7 @@ def elitismo_simples(populacao, fitness):
     zipado.sort(key=lambda x: x[1])
     return [x[0] for x in zipado[len(populacao)//2:]]
 
-def elitismo_composto(populacao, fitness):
+def composto(populacao, fitness):
     '''
     faz uma seleção ordenada mei doida onde metade da população atual vai
     para a próxima geração
@@ -32,3 +32,33 @@ def elitismo_composto(populacao, fitness):
     while len(selecionados) < metade:
         selecionados.add(random.choice(populacao)[1])
     return [populacao[i] for i in selecionados]
+
+def elitismo_simples(populacao, fitness):
+    '''
+    faz uma seleção ordenada pelo fitness onde metade da população atual vai
+    para a próxima geração
+    @param populacao: lista de individuos com o formato [objeto alocado, coordenadas]
+    @param fitness: lista de valores de fitness para cada individuo
+    @return: lista de individuos selecionados
+    '''
+    selecionados = []
+    for individuo in populacao:
+        selecionados.append(simples(individuo, fitness))
+    return selecionados
+
+def elitismo_composto(populacao, fitness):
+    '''
+    faz uma seleção ordenada mei doida onde metade da população atual vai
+    para a próxima geração
+    serão selecionados metade da população, dessa forma:
+    - 1/4 dos melhores
+    - 1/4 aleatórios, podendo ser dos piores ou melhores
+    sem elementos repetidos
+    @param populacao: lista de individuos com o formato [objeto alocado, coordenadas]
+    @param fitness: lista de valores de fitness para cada individuo
+    @return: lista de individuos selecionados
+    '''
+    selecionados = []
+    for individuo in populacao:
+        selecionados.append(composto(individuo, fitness))
+    return selecionados

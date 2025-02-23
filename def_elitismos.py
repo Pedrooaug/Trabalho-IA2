38996f65b24fc699c0cae1c4b258d3d2d0e4882b
xofiaxinha <msofia.lima@alu.ufc.br>
2025-02-23 14:28:35 -0300

    advinha o elitismo tava errado de novo 💋

--- def_elitismos.py
+++ def_elitismos.py
@@ -1,39 +1,5 @@
 import random
 
-def simples(populacao, fitness):
-    '''
-    faz uma seleção ordenada pelo fitness onde metade da população atual vai
-    para a próxima geração
-    @param populacao: lista de individuos com o formato [objeto alocado, coordenadas]
-    @param fitness: lista de valores de fitness para cada individuo
-    @return: lista de individuos selecionados
-    '''
-    zipado = list(zip(populacao, fitness))
-    zipado.sort(key=lambda x: x[1])
-    return [x[0] for x in zipado[:len(populacao)//2]]
-
-def composto(populacao, fitness):
-    '''
-    faz uma seleção ordenada mei doida onde metade da população atual vai
-    para a próxima geração
-    serão selecionados metade da população, dessa forma:
-    - 1/4 dos melhores
-    - 1/4 aleatórios, podendo ser dos piores ou melhores
-    sem elementos repetidos
-    @param populacao: lista de individuos com o formato [objeto alocado, coordenadas]
-    @param fitness: lista de valores de fitness para cada individuo
-    @return: lista de individuos selecionados
-    '''
-    zipado = list(zip(populacao, fitness))
-    zipado.sort(key=lambda x: x[1])
-    melhores = [x[0] for x in zipado[:(len(populacao)//4)]]
-    metade = len(populacao) // 2
-    selecionados = set(melhores)
-    while len(selecionados) < metade:
-        selecionados.add(random.choice(populacao))
-    
-    return list(selecionados)
-
 def elitismo_simples(populacao, fitness):
     '''
     faz uma seleção ordenada pelo fitness onde metade da população atual vai
@@ -42,10 +8,13 @@ def elitismo_simples(populacao, fitness):
     @param fitness: lista de valores de fitness para cada individuo
     @return: lista de individuos selecionados
     '''
-    selecionados = []
-    for individuo in populacao:
-        selecionados.append(simples(individuo, fitness))
-    return selecionados
+    #selecionados = []
+    #for individuo in populacao:
+     #   selecionados.append(simples(individuo, fitness))
+    #return selecionados
+    zipado = list(zip(populacao, fitness)) # junta todas as possíveis soluções com seus respectivos fitness
+    zipado.sort(key=lambda x: x[1]) # ordena as soluções pelo fitness
+    return [x[0] for x in zipado[:len(populacao)//2]] # retorna as soluções com os melhores fitness
 
 def elitismo_composto(populacao, fitness):
     '''
@@ -59,8 +28,12 @@ def elitismo_composto(populacao, fitness):
     @param fitness: lista de valores de fitness para cada individuo
     @return: lista de individuos selecionados
     '''
-    selecionados = []
-    for individuo in populacao:
-        selecionados.append(composto(individuo, fitness))
-    return selecionados
-    
+    zipado = list(zip(populacao, fitness))
+    zipado.sort(key=lambda x: x[1])
+    melhores = [x[0] for x in zipado[:(len(populacao)//4)]] # seleciona os 1/4 melhores
+    metade = len(populacao) // 2
+    selecionados = set(melhores)
+    while len(selecionados) < metade:
+        selecionados.add(random.choice(populacao)) # seleciona aleatoriamente os outros 1/4
+
+    return list(selecionados)
